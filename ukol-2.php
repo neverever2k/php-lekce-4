3ef18ae4b11d7a9d3aba7b6d3290af5e67d04523
Luděk Veselý <ludek.vesely@email.com>
2019-02-28 00:39:31 +0100

    add empty tables

---
+++ ukol-2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Úkol 2</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+<br>
+<div class="container">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Číslo řádku</th>
+                <th>Počet řádků</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>prvni sloupec</td>
+                <td>druhy sloupec</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
---
+++ ukol-3.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Úkol 3</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+<br>
+<div class="container">
+
+    <?php
+    $predmety = [
+        'zeměpis' => 1,
+        'dějepis' => 2,
+        'fyzika' => 3,
+        'přírodopis' => 1
+    ];
+    ?>
+
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th>Předmět</th>
+            <th>Známka</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>prvni sloupec</td>
+                <td>druhy sloupec</td>
+            </tr>
+        </tbody>
+    </table>
+
+    Průměr:
+
+</div>
+</body>
+</html>
